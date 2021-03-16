2bcb94b4a8445d1f00cae8da8540e8092c766d44
CebrinB <cebrin@byui.edu>
2021-03-15 22:37:13 -0500

    week11

--- test/main.js
+++ test/main.js
@@ -1,17 +1,36 @@
 
-//jQuery that retrieves the contents of the .txt file and loads into DOM object
-$.get("../text.json", function(data){
+//jQuery that retrieves the contents of a .json file and loads into DOM object
+$.get("text.json", function(data){
   $( "body" ).data("personData", data);
 }, 'json');
 
 
 function populateIndex() {
   //fill in homepage info
+  $("#brand").html($( "body" ).data( "personData" ).brand);
   $("#name").html($( "body" ).data( "personData" ).name);
   $("#jobtitle").html($( "body" ).data( "personData" ).jobtitle);
   $("#greeting").html($( "body" ).data( "personData" ).greeting);
   $("#whoami").html($( "body" ).data( "personData" ).whoami);
   $("#welcome").html($( "body" ).data( "personData" ).welcome);
+
+  //fill in footer links
+  $("#phone").html($( "body" ).data( "personData" ).phone);
+  $("#phone").attr('href', 'tel:' + ($( "body" ).data( "personData" ).phone));
+  $("#email").html($( "body" ).data( "personData" ).email);
+  $("#email").attr('href', 'mailto:' + ($( "body" ).data( "personData" ).email));
+  $('a[id^="facebook"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).facebook));
+  });
+  $('a[id^="twitter"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).twitter));
+  });
+  $('a[id^="linkedin"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).linkedin));
+  });
+  $('a[id^="instagram"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).instagram));
+  });
   
 }
 
@@ -19,15 +38,12 @@ function populateAbout() {
   //fill in about me info 
   $("#brand").html($( "body" ).data( "personData" ).brand);
   
-  //personal section
   $("#aboutme1").html($( "body" ).data( "personData" ).aboutme1);
   $("#aboutme2").html($( "body" ).data( "personData" ).aboutme2);
   
-  //work history section
   $("#title1").html($( "body" ).data( "personData" ).title1);
   $("#workhistory1").html($( "body" ).data( "personData" ).workhistory1);
   
-  //skills section
   $("#title2").html($( "body" ).data( "personData" ).title2);
   let divSkills = document.querySelectorAll('.skill');
   index = 0;
@@ -38,7 +54,6 @@ function populateAbout() {
     }
   );
 
-  //hobbies section
   $("#title3").html($( "body" ).data( "personData" ).title3);
   let divHobbies = document.querySelectorAll('.hobby');
   index = 0;
@@ -48,6 +63,24 @@ function populateAbout() {
       index++;
     }
   );
+
+  //fill in the footer links
+  $("#phone").html($( "body" ).data( "personData" ).phone);
+  $("#phone").attr('href', 'tel:' + ($( "body" ).data( "personData" ).phone));
+  $("#email").html($( "body" ).data( "personData" ).email);
+  $("#email").attr('href', 'mailto:' + ($( "body" ).data( "personData" ).email));
+  $('a[id^="facebook"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).facebook));
+  });
+  $('a[id^="twitter"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).twitter));
+  });
+  $('a[id^="linkedin"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).linkedin));
+  });
+  $('a[id^="instagram"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).instagram));
+  });
 }
 
 function populateProjects() {
@@ -99,37 +132,50 @@ function populateProjects() {
   $("#project9Title").html($( "body" ).data( "personData" ).project9Title);
   $("#project9Technologies").html($( "body" ).data( "personData" ).project9Technologies);
   
+
+  //fill in the footer links
+  $("#phone").attr('href', 'tel:' + ($( "body" ).data( "personData" ).phone));
+  $("#email").html($( "body" ).data( "personData" ).email);
+  $("#email").attr('href', 'mailto:' + ($( "body" ).data( "personData" ).email));
+  $('a[id^="facebook"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).facebook));
+  });
+  $('a[id^="twitter"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).twitter));
+  });
+  $('a[id^="linkedin"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).linkedin));
+  });
+  $('a[id^="instagram"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).instagram));
+  });
 }
 
 function populateContact() {
   //populate contact info
+  $("#brand").html($( "body" ).data( "personData" ).brand);
   $("#contactEmail").attr('action', $( "body" ).data( "personData" ).contactEmail);
+
+  //fill in the footer links
+  $("#phone").attr('href', 'tel:' + ($( "body" ).data( "personData" ).phone));
+  $("#email").html($( "body" ).data( "personData" ).email);
+  $("#email").attr('href', 'mailto:' + ($( "body" ).data( "personData" ).email));
+  $('a[id^="facebook"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).facebook));
+  });
+  $('a[id^="twitter"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).twitter));
+  });
+  $('a[id^="linkedin"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).linkedin));
+  });
+  $('a[id^="instagram"]').each(function() { 
+    $(this).attr('href', ($( "body" ).data( "personData" ).instagram));
+  });
 }
 
 window.addEventListener("load", () => {
   if ($( "body" ).data( "personData" )) {
-    
-    //populateAbout();
-
-
-
-    //fill in the nav and footer links
-    $("#brand").html($( "body" ).data( "personData" ).brand);
-    $("#phone").html($( "body" ).data( "personData" ).phone);
-    $("#phone").attr('href', 'tel:' + ($( "body" ).data( "personData" ).phone));
-    $("#email").html($( "body" ).data( "personData" ).email);
-    $("#email").attr('href', 'mailto:' + ($( "body" ).data( "personData" ).email));
-    $('a[id^="facebook"]').each(function() { 
-      $(this).attr('href', ($( "body" ).data( "personData" ).facebook));
-    });
-    $('a[id^="twitter"]').each(function() { 
-      $(this).attr('href', ($( "body" ).data( "personData" ).twitter));
-    });
-    $('a[id^="linkedin"]').each(function() { 
-      $(this).attr('href', ($( "body" ).data( "personData" ).linkedin));
-    });
-    $('a[id^="instagram"]').each(function() { 
-      $(this).attr('href', ($( "body" ).data( "personData" ).instagram));
-    });
+    populateAbout();
   } 
 });
